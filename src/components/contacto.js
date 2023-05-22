eabaaa2a8fa131fcd2c52b96b1b998434a8f0c8d
erwinfrancisco <erwin.fco@gmail.com>
2023-05-21 20:09:35 -0600

    Se añade Contacto y Ubicacion

--- src/components/contacto.js
+++ src/components/contacto.js
@@ -1,6 +1,39 @@
+import "./contacto.css"
+
+
 function Contacto(){
     return (
-        <div>Contacto</div>
+        <div>
+
+            <div>
+                
+                <p className="subtitulo-home">Nombre:</p>
+                <div className="contenedor-home">
+                <input className="inputs-contacto" type="text" placeholder="Escribe tu nombre..."></input>
+                </div>
+
+                <p className="subtitulo-home">Email:</p>
+                <div className="contenedor-home">
+                <input className="inputs-contacto" type="text" placeholder="Escribe tu email..."></input>
+                </div> 
+
+                <p className="subtitulo-home">Celular:</p>
+                <div className="contenedor-home">
+                <input className="inputs-contacto" type="text" placeholder="Celular..."></input>
+                </div> 
+
+                <p className="subtitulo-home">Mensaje:</p>
+                <div className="contenedor-home">
+                <input className="inputs-contacto2" type="text" placeholder="Mensaje..."></input>
+                </div> 
+                <div>
+                <input className="boton-enviar"type="submit" value="Enviar"></input>
+                </div>
+                
+            </div>
+            
+
+        </div>
     )
 }
 
--- src/components/ubicacion.js
+++ src/components/ubicacion.js
@@ -1,6 +1,29 @@
+import "./ubicacion.css"
+
+import Mapa from "./assets/ubicacion.png"
+
+
+
+
 function Ubicacion(){
     return (
-        <div>Ubicación</div>
+
+        <div>
+
+            <div>
+                <p className="subtitulo-ubicacion">Ubicación:</p>
+
+                <div className="contenedor-home">
+                   
+                    <img className="foto-ubicacion" src={Mapa}></img>
+                  
+                </div>          
+                
+            </div>
+            
+
+        </div>
+
     )
 }
 
